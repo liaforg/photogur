b7f66bd0a0b8557165981a8801b831c27478b621
Lia Forgione <liaforg@gmail.com>
2017-06-07 15:39:08 -0400

    editing pictures

--- app/controllers/pictures_controller.rb
+++ app/controllers/pictures_controller.rb
@@ -24,5 +24,19 @@ class PicturesController < ApplicationController
   def picture_params
     {title: params[:picture][:title], artist: params[:picture][:artist], url: params[:picture][:url]}
   end
-  
+
+  def edit
+    @picture = Picture.find(params[:id])
+  end
+
+  def update
+    @pictures = Picture.find(params[:id])
+    if @pictures.update_attributes(picture_params)
+      redirect_to "/pictures/#{@pictures.id}"
+    else
+      render :edit
+    end
+  end
+
+
 end
