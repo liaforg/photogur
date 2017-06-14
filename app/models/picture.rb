3e658e9a21ba671c6dea596b54df76f62cfef039
Lia Forgione <liaforg@gmail.com>
2017-06-14 09:57:01 -0400

    validations and error message

--- app/models/picture.rb
+++ app/models/picture.rb
@@ -1,2 +1,9 @@
 class Picture < ApplicationRecord
+
+
+    validates :artist, :url, presence: true
+    validates :title, length: {minimum: 3, maximum: 20}
+    validates :url, uniqueness: true
+
+    
 end
